ab417ee8868f537ed6f6745a5066bef7a49becb5
send2ugfd <send2ugfd@welgram.com>
2023-10-12 18:57:14 +0900

    Initial commit

---
+++ src/Day1/ex3.java
@@ -0,0 +1,23 @@
+package Day1;
+
+import java.util.Scanner;
+
+public class ex3 {
+    public static void main(String[] args) {
+        /*
+        * 문제 : 문자열 str과 정수 n이 주어집니다.
+        *        str이 n번 반복된 문자열을 만들어 출력하는 코드를 작성해 보세요.
+        * 제한사항 :
+        * 1 ≤ str의 길이 ≤ 10
+        * 1 ≤ n ≤ 5
+        * */
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            System.out.printf(str);
+        }
+    }
+}
---
+++ src/Day1/ex4.java
@@ -0,0 +1,22 @@
+package Day1;
+
+import java.util.Scanner;
+
+public class ex4 {
+    public static void main(String[] args) {
+        /*
+        * 문제 : 영어 알파벳으로 이루어진 문자열 str이 주어집니다. 각 알파벳을 대문자는 소문자로 소문자는 대문자로 변환해서 출력하는 코드를 작성해 보세요.
+        *
+        * 제한사항 :
+        * 1 ≤ str의 길이 ≤ 20
+            * str은 알파벳으로 이루어진 문자열입니다.
+         * */
+
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = a.toUpperCase();
+
+        System.out.println(b);
+
+    }
+}
