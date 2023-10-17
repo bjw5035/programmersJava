3673d8bfe3e52488906513a007fcfec5df6a6a88
bjw5035 <send2ugfd@naver.com>
2023-10-17 23:00:16 +0900

    수정

--- src/Level1/나머지가1이되는수찾기.java
+++ src/Level1/나머지가1이되는수찾기.java
@@ -12,13 +12,23 @@ public class 나머지가1이되는수찾기 {
 
     public int solution(int n) {
         int answer = 0;
-        int x = 0;
 
-//        if (n) {
-//
-//            System.out.println(n);
-//        }
+/*
 
+        while (true) {
+            if ((n & answer) == 1) {
+                break;
+            }
+            answer++;
+        }
+        return answer;
+*/
+        for (int i = 0; i < n; i++) {
+            if ((n & answer) == 1) {
+                break;
+            }
+            answer++;
+        }
         return answer;
     }
 
