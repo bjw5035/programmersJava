3a1a5824de11a2f79ebc7a8f322549e8a0c52357
bjw5035 <send2ugfd@naver.com>
2023-10-22 23:09:52 +0900

    수정

--- src/Level1/짝수와홀수.java
+++ src/Level1/짝수와홀수.java
@@ -12,6 +12,13 @@ public class 짝수와홀수 {
 
     public String solution(int num) {
         String answer = "";
-        return answer;
+
+        if (num % 2 == 0) {
+            return "Even";
+        } else {
+            return "Odd";
+        }
+//        return answer;
     }
+
 }
