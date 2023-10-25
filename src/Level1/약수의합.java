144a472488a12c90be7d16bdf307b14aaeb3aa4f
bjw5035 <send2ugfd@naver.com>
2023-10-25 23:07:31 +0900

    수정

--- src/Level1/약수의합.java
+++ src/Level1/약수의합.java
@@ -3,6 +3,16 @@ package Level1;
 public class 약수의합 {
     public int solution(int n) {
         int answer = 0;
+
+        if (n % n == 0) {
+            n = n + 1;
+            System.out.println(n);
+            return n;
+        }
         return answer;
+    }
+
+    public static void main(String[] args) {
+
     }
 }
