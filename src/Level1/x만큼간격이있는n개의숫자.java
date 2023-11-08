c445a610dbaf580f0ae08b8f87f16a222abda49b
jaewoo <send2ugfd@welgram.com>
2023-11-08 17:29:36 +0900

    수정

--- src/Level1/x만큼간격이있는n개의숫자.java
+++ src/Level1/x만큼간격이있는n개의숫자.java
@@ -12,6 +12,10 @@ public class x만큼간격이있는n개의숫자 {
 
     public long[] solution(int x, int n) {
         long[] answer = {};
+
+        for (int i = 0; i < x; i++) {
+
+        }
         return answer;
     }
 }
