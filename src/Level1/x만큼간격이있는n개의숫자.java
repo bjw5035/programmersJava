5a61160a27d29070bdc7c8d815e6afe180601988
bjw5035 <send2ugfd@naver.com>
2023-11-22 22:22:27 +0900

    수정

--- src/Level1/x만큼간격이있는n개의숫자.java
+++ src/Level1/x만큼간격이있는n개의숫자.java
@@ -3,7 +3,9 @@ package Level1;
 public class x만큼간격이있는n개의숫자 {
     /*
     * 문제 설명
-    * 함수 solution은 정수 x와 자연수 n을 입력 받아, x부터 시작해 x씩 증가하는 숫자를 n개 지니는 리스트를 리턴해야 합니다. 다음 제한 조건을 보고, 조건을 만족하는 함수, solution을 완성해주세요.
+    * 함수 solution은 정수 x와 자연수 n을 입력 받아,
+    * x부터 시작해 x씩 증가하는 숫자를 n개 지니는 리스트를 리턴해야 합니다.
+    * 다음 제한 조건을 보고, 조건을 만족하는 함수, solution을 완성해주세요.
     *
     * 제한 조건
     * x는 -10000000 이상, 10000000 이하인 정수입니다.
@@ -11,7 +13,8 @@ public class x만큼간격이있는n개의숫자 {
     * */
 
     public long[] solution(int x, int n) {
-        long[] answer = {};
+        long[] answer = new long[x];
+        long num = x;
 
         for (int i = 0; i < x; i++) {
 
