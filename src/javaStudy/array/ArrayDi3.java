e044d879f23ee818e278ac67c3889ca9f7eb9da2
bjw5035 <send2ugfd@naver.com>
2024-08-05 23:00:40 +0900

    feat : 인프런 자바입문 강의 예제 추가(배열, 향상된 for문 강의)

---
+++ src/javaStudy/array/ArrayDi3.java
@@ -0,0 +1,22 @@
+package javaStudy.array;
+
+public class ArrayDi3 {
+
+    public static void main(String[] args) {
+
+        // 2x3 2차원 배열을 만든다.
+        int[][] arr = {
+            {1,2,3},
+            {4,5,6}
+        }; // 행2, 열3
+
+        for (int row = 0; row < arr.length; row++) {
+            for (int colum = 0; colum < arr[row].length; colum++) {
+                System.out.print(arr[row][colum] + " ");
+            }
+            System.out.println(); // 한 행이 끝나면 라인을 변경한다.
+        }
+
+    }
+
+}
---
+++ src/javaStudy/array/ArrayDi4.java
@@ -0,0 +1,26 @@
+package javaStudy.array;
+
+public class ArrayDi4 {
+
+    public static void main(String[] args) {
+
+        // 2x3 2차원 배열을 만든다.
+        int[][] arr = new int[2][3];
+
+        int i = 1;
+        for (int row = 0; row < arr.length; row++) {
+            for (int colum = 0; colum < arr[row].length; colum++) {
+                arr[row][colum] = i++;
+            }
+        }
+
+        for (int row = 0; row < arr.length; row++) {
+            for (int colum = 0; colum < arr[row].length; colum++) {
+                System.out.print(arr[row][colum] + " ");
+            }
+            System.out.println(); // 한 행이 끝나면 라인을 변경한다.
+        }
+
+    }
+
+}
